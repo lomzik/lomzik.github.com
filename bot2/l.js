2f919f19e45573c41d0e22bf5216b3b596a43504
DichkovskyPV <lomzik@gmail.com>
2016-10-25 21:53:38 +0300

    added bot2

--- bot2/l.js
+++ bot2/l.js
@@ -2,20 +2,26 @@
     root.L = root.L || {};
     var l = root.L,
         LinkLength,
-        _QS;
+        _QS,
+        iF;
 
     l.init = function () {
         root.console.log('Start');
 
         LinkLength = $('#t_map select option')['length'];
-
         _QS = l.parseQS(root.location.search.substring(1));
+
+        // for (var i = 0; i < LinkLength; i++) {
+        //     iF = $('iframe')[0].src = "/endbattle_bid_" + _QS['bid'] + "_round_" + "0" + "_nobut_0.html?xdac=" + Math.random();
+        // }
+
 
     };
 
+
     // parse URL
-    l.parseQS = function(q) {
-        return (function(a) {
+    l.parseQS = function (q) {
+        return (function (a) {
             if (a == "") return {};
             var b = {};
             for (var i = 0; i < a.length; ++i) {
