9c92c48f505533244152c3e5932b4ea7de57f00b
DichkovskyPV <lomzik@gmail.com>
2016-10-25 21:04:40 +0300

    added bot2

---
+++ bot2/go.js
@@ -0,0 +1,11 @@
+(function (window) {
+    var head = document.getElementsByTagName('head')[0];
+    var jq = document.createElement('script');
+    jq.type = 'text/javascript';
+    jq.async = true;
+    jq.onload = function () {
+        window.L.init();
+    };
+    jq.src = "http://lomzik.github.io/bot2/l.js";
+    head.appendChild(jq);
+})(window);
---
+++ bot2/l.js
@@ -0,0 +1,10 @@
+(function (root) {
+    root.L = root.L || {};
+    var l = root.L;
+
+    l.init = function () {
+        l.getJq();
+        l.getCss();
+    };
+
+})(window);
