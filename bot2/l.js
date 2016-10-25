36852b88c7d8afacb9f6c2b36b8acfbde6ca7fc0
DichkovskyPV <lomzik@gmail.com>
2016-10-25 21:24:20 +0300

    added bot2

--- bot2/l.js
+++ bot2/l.js
@@ -1,9 +1,23 @@
 (function (root) {
     root.L = root.L || {};
-    var l = root.L;
+    var l = root.L,
+        LinkLength,
+        _url;
 
     l.init = function () {
         root.console.log('Start');
+
+        LinkLength = $('#t_map select option')['length'];
+
+        _url = l.parseURL(root.location.href);
+
     };
+
+    // parse URL
+    l.parseURL = function (str) {
+        var url = document.createElement('a');
+        url.href = str;
+        return url;
+    }
 
 })(window);
